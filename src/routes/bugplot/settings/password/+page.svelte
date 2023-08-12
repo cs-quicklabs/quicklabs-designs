33f92e1f0fb87587041b5e70d8bff2cc6c994d83
Aashish Dhawan <aashishdhawan@gmail.com>
2023-08-12 17:15:13 +0530

    updated settings design

--- src/routes/bugplot/settings/password/+page.svelte
+++ src/routes/bugplot/settings/password/+page.svelte
@@ -6,35 +6,11 @@
 <div class="mx-auto max-w-7xl pt-16 lg:flex lg:gap-x-16 lg:px-8">
     <ProfileSidebar/>
     <div class="">
-        <h2 class="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-        <p class="mt-1 text-sm leading-6 text-gray-600">Update your personal information.</p>
+        <h2 class="text-base font-semibold leading-7 text-gray-900">Change Password</h2>
+        <p class="mt-1 text-sm leading-6 text-gray-600">Change your old password and set a new one.</p>
         <div class="mt-4 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6">
-            <div class="sm:col-span-3">
-                <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
-                <div class="mt-2">
-                    <input
-                        type="text"
-                        name="first-name"
-                        id="first-name"
-                        autocomplete="given-name"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    >
-                </div>
-            </div>
-            <div class="sm:col-span-3">
-                <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
-                <div class="mt-2">
-                    <input
-                        type="text"
-                        name="last-name"
-                        id="last-name"
-                        autocomplete="family-name"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    >
-                </div>
-            </div>
             <div class="col-span-full   ">
-                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Old Password</label>
                 <div class="mt-2">
                     <input
                         id="email"
@@ -46,7 +22,7 @@
                 </div>
             </div>
             <div class="col-span-full   ">
-                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Organization Name</label>
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">New Password</label>
                 <div class="mt-2">
                     <input
                         id="email"
@@ -58,19 +34,7 @@
                 </div>
             </div>
             <div class="col-span-full   ">
-                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Role</label>
-                <div class="mt-2">
-                    <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autocomplete="email"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    >
-                </div>
-            </div>
-            <div class="col-span-full   ">
-                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Language</label>
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
                 <div class="mt-2">
                     <input
                         id="email"
