6dda49648081d53ab380093e4904e0f1d32ee06b
Aashish Dhawan <aashishdhawan@gmail.com>
2023-08-19 16:34:16 +0530

    added milestones layout

--- src/routes/bugplot/projects/[slug]/milestones/+page.svelte
+++ src/routes/bugplot/projects/[slug]/milestones/+page.svelte
@@ -4,3 +4,58 @@
 </script>
 <Fullnavbar/>
 <ProjectHeader selection="Milestones"/>
+<!-- 3 column wrapper -->
+  <div class="mx-auto w-full grow lg:flex">
+    <!-- Left sidebar & main wrapper -->
+    <div class="flex-1 xl:flex">
+      <div class="xl:flex-1">
+        <!-- Main area -->
+        <div class="flow-root">
+    <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+        <div class="overflow-hidden ">
+          <table class="min-w-full divide-y divide-gray-300">
+            <thead class="">
+              <tr>
+                <th scope="col" class="py-2.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
+                <th scope="col" class="px-3 py-2.5 text-left text-sm font-semibold text-gray-900">Title</th>
+                <th scope="col" class="px-3 py-2.5 text-left text-sm font-semibold text-gray-900">Email</th>
+                <th scope="col" class="px-3 py-2.5 text-left text-sm font-semibold text-gray-900">Role</th>
+                <th scope="col" class="relative py-2.5 pl-3 pr-4 sm:pr-6">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200 bg-white">
+              <tr>
+                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">Lindsay Walton</td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Front-end Developer</td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">lindsay.walton@example.com</td>
+                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
+                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                  <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
+                </td>
+              </tr>
+
+              <!-- More people... -->
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+      </div>
+    </div>
+
+    <div class="shrink-0 border-t border-gray-200 px-4 py-6 sm:px-6 lg:w-96 lg:border-l lg:border-t-0 lg:pr-8 xl:pr-6 h-screen">
+      <!-- Right column area -->
+        
+          <h3 class="text-base font-semibold leading-6 text-gray-900">Manage Milestones</h3>
+          <div class="mt-2 max-w-xl text-sm text-gray-500">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae voluptatibus corrupti atque repudiandae nam.</p>
+          </div>
+          <div class="mt-5">
+            <button type="button" class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Add Milestone</button>
+          </div>
+    </div>
+  </div>
