56c0aa7109064ba7f18ac1ef5a8dcbabdde314fb
Aashish Dhawan <aashishdhawan@gmail.com>
2023-08-19 20:03:17 +0530

    update milestone UI

--- src/routes/bugplot/projects/[slug]/milestones/+page.svelte
+++ src/routes/bugplot/projects/[slug]/milestones/+page.svelte
@@ -12,30 +12,30 @@
         <!-- Main area -->
         <div class="flow-root">
     <div class="-my-2 overflow-x-auto ">
-      <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+      <div class="inline-block min-w-full py-3 align-middle sm:px-6 lg:px-8">
         <div class="overflow-hidden ">
           <table class="min-w-full divide-y divide-gray-300">
             <tbody class="divide-y divide-gray-200 bg-white ml-4">
                 <tr>
-                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0 w-full">Milestone 0 </td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">1 active Test runs</td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Due on 12 Jan, 2024</td>
-                    <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                    <td class="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0 w-full">Milestone 0 </td>
+                    <td class="whitespace-nowrap px-3 py-3 text-sm text-gray-500">1 active Test runs</td>
+                    <td class="whitespace-nowrap px-3 py-3 text-sm text-gray-500">Due on 12 Jan, 2024</td>
+                    <td class="whitespace-nowrap px-3 py-3 text-sm text-gray-500">
                         <span class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-700 ring-1 ring-inset ring-yellow-600/20"> In Progress</span>
                       </td>
-                            <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                            <td class="relative whitespace-nowrap py-3 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                       <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
                     </td>
                   </tr>
                 {#each Array(5) as _, index (index)}
                 <tr>
-                    <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0 w-full">Milestone 1 </td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">No Active test runs</td>
-                    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Completed on 12 Jan, 2024</td>
-                    <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                    <td class="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0 w-full">Milestone 1 </td>
+                    <td class="whitespace-nowrap px-3 py-3 text-sm text-gray-500">No Active test runs</td>
+                    <td class="whitespace-nowrap px-3 py-3 text-sm text-gray-500">Completed on 12 Jan, 2024</td>
+                    <td class="whitespace-nowrap px-3 py-3 text-sm text-gray-500">
                         <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Completed</span>
                       </td>
-                            <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                            <td class="relative whitespace-nowrap py-3 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                     </td>
                   </tr>
                 {/each}
