5b79a11b7cc737c122e850676be0e48676a28412
Aashish Dhawan <aashishdhawan@gmail.com>
2024-02-11 13:21:07 +0530

    minor change

--- src/routes/quick-release/changelog/new/+page.svelte
+++ src/routes/quick-release/changelog/new/+page.svelte
@@ -410,7 +410,7 @@
 						datepicker-autohide
 						type="text"
 						class=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-						placeholder="Select date" />
+						placeholder="Select date and time" />
 				</div>
 			</div>
 			<div class="flex justify-end gap-x-3">
