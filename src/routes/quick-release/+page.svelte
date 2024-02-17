56532143807b004f487c106a86111fd1ec10bb67
Aashish Dhawan <aashishdhawan@gmail.com>
2024-02-17 16:37:16 +0530

    Ui change

--- src/routes/quick-release/+page.svelte
+++ src/routes/quick-release/+page.svelte
@@ -34,7 +34,10 @@
 		{ title: 'Login', screens: login },
 		{ title: 'Signup', screens: signup },
 		{ title: 'Changelog', screens: changelog },
-		{ title: 'Profile Settings', screens: profile },
+		{ title: 'Profile Settings', screens: profile }
+	];
+
+	let workflows2 = [
 		{ title: 'Account Settings', screens: account },
 		{ title: 'Team Settings', screens: team }
 	];
@@ -60,6 +63,37 @@
 									<div
 										class="relative flex items-center space-x-3 px-6 py-2 focus-within:ring-2 focus-within:ring-inset
 								focus-within:ring-indigo-500 hover:bg-gray-50">
+										<div class="min-w-0 flex-1">
+											<a href={screen.link} class="focus:outline-none">
+												<p class="text-sm font-medium text-gray-900">{screen.title}</p>
+											</a>
+										</div>
+									</div>
+								</li>
+							{/each}
+						</ul>
+					{/each}
+				</div>
+				<!-- End left column area -->
+			</div>
+		</div>
+
+		<div
+			class="border-b border-gray-200 bg-white xl:w-64 xl:flex-shrink-0 xl:border-b-0 xl:border-r xl:border-gray-200">
+			<div class="h-full sm:pl-6 lg:pl-8 xl:pl-0">
+				<!-- Start left column area -->
+				<div class="relative">
+					{#each workflows2 as workflow}
+						<div
+							class="sticky top-0 z-10 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
+							<h3>{workflow.title}</h3>
+						</div>
+						<ul role="list" class="relative z-0 divide-y divide-gray-200">
+							{#each workflow.screens as screen}
+								<li class="bg-white">
+									<div
+										class="relative flex items-center space-x-3 px-6 py-2 focus-within:ring-2 focus-within:ring-inset
+							focus-within:ring-indigo-500 hover:bg-gray-50">
 										<div class="min-w-0 flex-1">
 											<a href={screen.link} class="focus:outline-none">
 												<p class="text-sm font-medium text-gray-900">{screen.title}</p>
