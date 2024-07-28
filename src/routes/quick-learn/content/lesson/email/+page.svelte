63ea9b45b2db0ef059fd88c4d57703bb857c27b0
Aashish Dhawan <aashishdhawan@gmail.com>
2024-07-28 17:09:06 +0530

    lesson details

---
+++ src/routes/quick-learn/content/lesson/email/+page.svelte
@@ -0,0 +1,150 @@
+<main>
+	<div class="bg-gray-50 relative z-0 flex-1 overflow-x-scroll focus:outline-none h-screen">
+		<div class="px-4 pt-16 sm:flex sm:items-center sm:justify-center sm:px-6 lg:px-8">
+			<nav class="flex" aria-label="Breadcrumb">
+				<ol class="inline-flex justify-self-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+					<li class="inline-flex items-center">
+						<a
+							href="/quick-learn/team/1/roadmaps"
+							class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white hover:underline">
+							<svg
+								class="w-3 h-3 me-2.5"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="currentColor"
+								viewBox="0 0 20 20">
+								<path
+									d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+							</svg>
+							Web Development Roadmaps
+						</a>
+					</li>
+					<li>
+						<div class="flex items-center">
+							<svg
+								class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 6 10">
+								<path
+									stroke="currentColor"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="m1 9 4-4-4-4" />
+							</svg>
+							<a
+								href="/quick-learn/team/1/courses"
+								class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white hover:underline"
+								>Github Course</a>
+						</div>
+					</li>
+				</ol>
+			</nav>
+		</div>
+		<div class="px-4 mb-8 sm:flex sm:items-center sm:justify-center sm:px-6 lg:px-8">
+			<div class="items-baseline">
+				<h1 class="text-5xl font-extrabold leading-tight">Cherry-Pick Command and its usage</h1>
+				<p
+					class="mt-1 ml-1 text-sm text-gray-500 truncate sm:flex sm:items-center sm:justify-center">
+					Aashish Dhawan added this lesson on June 12, 2024
+				</p>
+			</div>
+		</div>
+		<article
+			class="flex justify-center mx-auto w-full max-w-5xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+			<img
+				src="/SCR-20240728-ouda-2.png"
+				alt="Cherry-Pick Command and its usage"
+				class="max-w-5xl object-cover" />
+		</article>
+	</div>
+</main>
+
+<!-- Main modal -->
+<div
+	id="defaultModal"
+	tabindex="-1"
+	aria-hidden="true"
+	class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+	<div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+		<!-- Modal content -->
+		<div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+			<!-- Modal header -->
+			<div
+				class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+				<h3 class="text-lg font-semibold text-gray-900 dark:text-white">Create New Roadmap</h3>
+				<button
+					type="button"
+					class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+					data-modal-toggle="defaultModal">
+					<svg
+						aria-hidden="true"
+						class="w-5 h-5"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg"
+						><path
+							fill-rule="evenodd"
+							d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+							clip-rule="evenodd"></path
+						></svg>
+					<span class="sr-only">Close modal</span>
+				</button>
+			</div>
+			<!-- Modal body -->
+			<form action="#">
+				<div class="grid gap-4 mb-4 sm:grid-cols-1">
+					<div class="sm:col-span-2">
+						<label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+							>Name</label>
+						<input
+							type="text"
+							name="name"
+							id="name"
+							class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+							placeholder="Type roadmap name"
+							required="" />
+					</div>
+					<div class="sm:col-span-2">
+						<label
+							for="category"
+							class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+							>Roadmap Category</label>
+						<select
+							id="category"
+							class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+							<option selected="">Select category</option>
+							<option value="TV">HR</option>
+							<option value="PC">Sales</option>
+							<option value="GA">Engineering</option>
+							<option value="PH">Recruitment</option>
+						</select>
+					</div>
+					<div class="sm:col-span-2">
+						<label
+							for="description"
+							class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+							>Description</label>
+						<textarea
+							id="description"
+							rows="4"
+							class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+							placeholder="Write roadmap description here"></textarea>
+					</div>
+				</div>
+				<button
+					type="submit"
+					class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+					Create New Roadmap
+				</button>
+				<button
+					data-modal-toggle="defaultModal"
+					class="py-2.5 px-5 ml-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+					Cancel
+				</button>
+			</form>
+		</div>
+	</div>
+</div>
