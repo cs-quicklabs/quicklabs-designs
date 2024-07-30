d047923cedbbf281a6189bc961ee5d2a70d0a44a
Aashish Dhawan <aashishdhawan@gmail.com>
2024-07-30 21:49:04 +0530

    fix ui

--- src/routes/quick-learn/team/1/+page.svelte
+++ src/routes/quick-learn/team/1/+page.svelte
@@ -472,6 +472,428 @@
 	</div>
 </div>
 
+<!-- Main modal -->
+<form
+	action="#"
+	method="get"
+	id="defaultModal"
+	tabindex="-1"
+	aria-hidden="true"
+	class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full justify-center items-center">
+	<div class="relative w-full h-full max-w-4xl md:h-auto">
+		<!-- Modal content -->
+		<div class="relative bg-white rounded-lg shadow dark:bg-gray-800">
+			<!-- Modal header -->
+			<div class="flex items-start justify-between p-4 rounded-t">
+				<h3 class="text-xl font-semibold text-gray-900 dark:text-gray-400">Assign New Roadmap</h3>
+				<button
+					type="button"
+					class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+					data-modal-toggle="defaultModal">
+					<svg
+						aria-hidden="true"
+						class="w-5 h-5"
+						fill="currentColor"
+						viewBox="0 0 20 20"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							fill-rule="evenodd"
+							d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+							clip-rule="evenodd"></path>
+					</svg>
+					<span class="sr-only">Close modal</span>
+				</button>
+			</div>
+			<!-- Modal body -->
+			<div class="px-4 md:px-6">
+				<div class="mb-4 border-b border-gray-200 dark:border-gray-700">
+					<ul
+						class="flex flex-wrap -mb-px text-sm font-medium text-center"
+						id="myTab"
+						data-tabs-toggle="#myTabContent"
+						role="tablist">
+						<li class="mr-1" role="presentation">
+							<span class="inline-block pb-2 pr-1 text-gray-700" id="brand-tab">
+								Select Roadmaps
+							</span>
+						</li>
+					</ul>
+				</div>
+				<div id="myTabContent">
+					<div
+						class="grid grid-cols-2 gap-4 md:grid-cols-3"
+						id="brand"
+						role="tabpanel"
+						aria-labelledby="brand-tab">
+						<div class="space-y-2">
+							<h5 class="text-lg font-medium text-black dark:text-white">Engineering</h5>
+
+							<div class="flex items-center">
+								<input
+									id="apple"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="apple"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Web Development Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="asus"
+									type="checkbox"
+									value=""
+									checked
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label for="asus" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Backend Development Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="acer"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label for="acer" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Frontend Development Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="allview"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="allview"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Learning Clean Code Roadmap
+								</label>
+							</div>
+						</div>
+
+						<div class="space-y-2">
+							<h5 class="text-lg font-medium text-black dark:text-white">HR</h5>
+
+							<div class="flex items-center">
+								<input
+									id="beats"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="beats"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									HR Learning Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="bose"
+									type="checkbox"
+									value=""
+									checked
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label for="bose" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Recruitment Learning Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="benq"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label for="benq" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Team Leadership Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="bosch"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="bosch"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Onboarding Learning Roadmap
+								</label>
+							</div>
+						</div>
+
+						<div class="space-y-2">
+							<h5 class="text-lg font-medium text-black dark:text-white">Sales</h5>
+
+							<div class="flex items-center">
+								<input
+									id="canon"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="canon"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Cold Calling Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="cisco"
+									type="checkbox"
+									value=""
+									checked
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="cisco"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Account Nurturing Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="cowon"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="cowon"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Email Marketing Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="clevo"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="clevo"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Other Roadmap
+								</label>
+							</div>
+						</div>
+						<div class="space-y-2 mt-4">
+							<h5 class="text-lg font-medium text-black dark:text-white">Engineering</h5>
+
+							<div class="flex items-center">
+								<input
+									id="apple"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="apple"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Web Development Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="asus"
+									type="checkbox"
+									value=""
+									checked
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label for="asus" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Backend Development Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="acer"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label for="acer" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Frontend Development Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="allview"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="allview"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Learning Clean Code Roadmap
+								</label>
+							</div>
+						</div>
+
+						<div class="space-y-2 mt-4">
+							<h5 class="text-lg font-medium text-black dark:text-white">HR</h5>
+
+							<div class="flex items-center">
+								<input
+									id="beats"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="beats"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									HR Learning Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="bose"
+									type="checkbox"
+									value=""
+									checked
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label for="bose" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Recruitment Learning Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="benq"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label for="benq" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Team Leadership Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="bosch"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="bosch"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Onboarding Learning Roadmap
+								</label>
+							</div>
+						</div>
+
+						<div class="space-y-2 mt-4">
+							<h5 class="text-lg font-medium text-black dark:text-white">Sales</h5>
+
+							<div class="flex items-center">
+								<input
+									id="canon"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="canon"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Cold Calling Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="cisco"
+									type="checkbox"
+									value=""
+									checked
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="cisco"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Account Nurturing Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="cowon"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="cowon"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Email Marketing Roadmap
+								</label>
+							</div>
+
+							<div class="flex items-center">
+								<input
+									id="clevo"
+									type="checkbox"
+									value=""
+									class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+
+								<label
+									for="clevo"
+									class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+									Other Roadmap
+								</label>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Modal footer -->
+			<div class="flex items-center p-6 space-x-4 rounded-b dark:border-gray-600">
+				<button
+					data-modal-toggle="defaultModal"
+					class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-700 dark:hover:bg-primary-800 dark:focus:ring-primary-800">
+					Save
+				</button>
+				<button
+					data-modal-toggle="defaultModal"
+					class="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+					Cancel
+				</button>
+			</div>
+		</div>
+	</div>
+</form>
+
 <div
 	id="user-activities-modal"
 	tabindex="-1"
