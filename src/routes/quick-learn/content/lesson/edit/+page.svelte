1d986680c301824f0d45b60bf13bc838fd1ab792
Aashish Dhawan <aashishdhawan@gmail.com>
2024-08-10 21:01:34 +0530

    Added header for lessons editor

--- src/routes/quick-learn/content/lesson/edit/+page.svelte
+++ src/routes/quick-learn/content/lesson/edit/+page.svelte
@@ -43,6 +43,15 @@
 				</ol>
 			</nav>
 		</div>
+		<div class="px-4 mb-8 sm:flex sm:items-center sm:justify-center sm:px-6 lg:px-8">
+			<div class="items-baseline">
+				<h1 class="text-5xl font-extrabold leading-tight">Cherry-Pick Command and its usage</h1>
+				<p
+					class="mt-1 ml-1 text-sm text-gray-500 truncate sm:flex sm:items-center sm:justify-center">
+					Aashish Dhawan added this lesson on June 12, 2024
+				</p>
+			</div>
+		</div>
 
 		<article
 			class="flex justify-center mx-auto w-full max-w-5xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
