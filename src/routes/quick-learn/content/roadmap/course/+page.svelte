cd18e0e7548123a4c5452fc8b9fb53cfa3144435
Aashish Dhawan <aashishdhawan@gmail.com>
2024-08-11 15:11:07 +0530

    Added pending approval card

--- src/routes/quick-learn/content/roadmap/course/+page.svelte
+++ src/routes/quick-learn/content/roadmap/course/+page.svelte
@@ -377,6 +377,35 @@
 							</div>
 						</a>
 					</li>
+					<li
+						class="col-span-1 rounded-lg shadow-sm hover:shadow-lg border-gray-100 bg-gray-100 group"
+						id="space_8">
+						<a href="/quick-learn/content/lesson/mine">
+							<div class="flex-wrap py-4 px-6 text-gray-900 h-48">
+								<h1 id="message-heading" class="font-medium text-gray-900 group-hover:underline">
+									Inserting Pictures and Styling
+								</h1>
+								<p class="font-normal text-sm text-gray-500 line-clamp-3 mt-2">
+									Learn Git basics in this course. This shows first three lines of the content and
+									whatever lesson contains
+								</p>
+								<div class="items-center space-x-1 py-3 flex font-medium text-yellow-500">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 24 24"
+										fill="currentColor"
+										class="size-4">
+										<path
+											fill-rule="evenodd"
+											d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+											clip-rule="evenodd" />
+									</svg>
+
+									<p class="text-xs text-gray-400 line-clamp-2">Pending for approval</p>
+								</div>
+							</div>
+						</a>
+					</li>
 				</ul>
 			</div>
 		</div>
