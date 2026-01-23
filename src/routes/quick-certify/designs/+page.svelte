35f2e50ff65144173a0eca3bdd0effe4e3c76826
divanshu2805 <divanshu@crownstack.com>
2026-01-23 19:16:27 +0530

    change name

--- src/routes/quick-certify/designs/+page.svelte
+++ src/routes/quick-certify/designs/+page.svelte
@@ -153,7 +153,7 @@
     <!-- Certificates -->
     <div class="flex items-center">
     <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-neutral-primary border-default-medium bg-neutral-secondary-medium rounded-full checked:border-brand focus:ring-2 focus:outline-none focus:ring-brand-subtle border border-default appearance-none">
-    <label for="default-radio-1" class="select-none ms-2 text-sm font-medium text-heading">Default radio</label>
+    <label for="default-radio-1" class="select-none ms-2 text-sm font-medium text-heading">Certificate</label>
 </div>
 
     <!-- Show All -->
